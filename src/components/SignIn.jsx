6ea50c6df98037309df54c5318e822ced2dd3555
Cedric Fonsat <cedricfonsat@MacBook-Air-de-Cedric.local>
2023-12-23 20:43:09 +0100

    add google auth

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -28,7 +28,7 @@ const SignIn = ({signInGoogle}) => {
           </div>
           <button
             className="h-10 px-6 font-semibold rounded-md bg-black text-white"
-            type="submit"
+            type="button"
             onClick={signInGoogle}
           >
             Sign In With Google
